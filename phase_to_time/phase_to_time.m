f77d39ca9ea8883a4fc4ff7dd74054dacfca87d0
Jean-Michel Friedt <friedtj@free.fr>
2025-09-08 16:32:39 +0200

    phase-time conversion

---
+++ phase_to_time/phase_to_time.m
@@ -0,0 +1,17 @@
+fc=1e7;
+N=100;
+fF=  [1    10  100 1000 10000 1e5  1e6  1e7];
+SdBc=[-70 -70  -95 -120 -130 -130 -130 -130]
+
+SdBcint=[];
+fFint=[];
+for k=1:length(SdBc)-1
+   SdBcint=[SdBcint SdBc(k)+(SdBc(k+1)-SdBc(k))/(fF(k+1)-fF(k))*(linspace((fF(k)),(fF(k+1)),N)-fF(k))];
+   fFint=[fFint logspace(log10(fF(k)),log10(fF(k+1)),N)];
+end
+
+SdBrad=SdBcint+3;   % dBc/Hz -> dBrad^2/Hz
+rad=sqrt(10.^(SdBrad(1:end-1)/10).*(fFint(2:end)-fFint(1:end-1))); % dBrad^2/Hz -> rad
+res=rad/2/pi/fc     % rad -> s
+sqrt(sum(res.^2))   % integrate noise fluctuations
+semilogx(fFint,(SdBcint))
