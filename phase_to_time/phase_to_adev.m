d08597307b35a4afce9d5ae6d1f8d4dea6d7d33d
Jean-Michel Friedt <friedtj@free.fr>
2025-09-13 14:38:41 +0200

    compute adev for all phase noise cases

--- phase_to_time/phase_to_adev.m
+++ phase_to_time/phase_to_adev.m
@@ -1,35 +1,46 @@
 fc=1e7; % carrier frequency
 N=100;  % interpolation factor
 % phase noise spectral SdBc @ fF
-fF=  [1    10  100 1000 10000 1e5] %  1e6  1e7];
-SdBc=[-70 -70  -95 -120 -130 -130] % -130 -130]
+fF=  [1    10  100 1000 10000 1e5] %
+SdBc(:,1)=[-70  -70  -95  -120 -130 -130]; % BC/GM class I
+SdBc(:,2)=[-100 -100 -115 -130 -140 -140]; % GM class II (LJ)
+SdBc(:,3)=[-90  -90  -107 -125 -135 -135]  % BC class II
+
+
 
 %%%%%%%% interpolate, convert SdBc to SdBrad to Sy (linear) & integrate to AVAR
-SdBcint=[];
-fFint=[];
-for k=1:length(SdBc)-1
-   SdBcint=[SdBcint SdBc(k)+(SdBc(k+1)-SdBc(k))/(fF(k+1)-fF(k))*(linspace((fF(k)),(fF(k+1)),N)-fF(k))];
-   fFint=[fFint logspace(log10(fF(k)),log10(fF(k+1)),N)];
+for count=1:3
+  SdBcint=[];
+  fFint=[];
+  for k=1:length(SdBc(:,count))-1
+     SdBcint=[SdBcint SdBc(k,count)+(SdBc(k+1,count)-SdBc(k,count))/(fF(k+1)-fF(k))*(linspace((fF(k)),(fF(k+1)),N)-fF(k))];
+     fFint=[fFint logspace(log10(fF(k)),log10(fF(k+1)),N)];
+  end
+  
+  SdBrad=SdBcint+3;   % dBc/Hz -> dBrad^2/Hz
+  Sy=(10.^(SdBrad/10)).*(fFint.^2);
+  if (count==1)
+    subplot(211)
+    semilogx(fFint,(SdBrad))
+    hold on
+    semilogx(fFint,10*log10(Sy))
+    xlabel('Fourier frequency (Hz)')
+    ylabel('phase noise (dBc/Hz)');legend('Sphi','Sy=Sphixf^2','location','southwest')
+    grid on
+    title('Class I')
+  end
+  subplot(212)
+  m=1;
+  for tau=logspace(-2,2,1024)
+    avar(m)=2*sum(Sy(1:end-1).*(fFint(2:end)-fFint(1:end-1)).*sin(4*pi*tau*fFint(1:end-1)).^4./(4*pi*tau*fFint(1:end-1)).^2);
+    m=m+1;
+  end
+  
+  % plot resulting adev=sqrt(avar)
+  adev=sqrt(avar);
+  loglog(logspace(-2,2,1024),adev/fc)
+  xlabel('tau (s)');ylabel('ADEV (no unit)')
+  grid on
+  hold on
 end
-
-subplot(211)
-SdBrad=SdBcint+3;   % dBc/Hz -> dBrad^2/Hz
-Sy=(10.^(SdBrad/10)).*(fFint.^2);
-semilogx(fFint,(SdBrad))
-hold on
-semilogx(fFint,10*log10(Sy))
-xlabel('Fourier frequency (Hz)')
-ylabel('phase noise (dBc/Hz)');legend('Sphi','Sy=Sphixf^2','location','southwest')
-grid on
-subplot(212)
-m=1
-for tau=logspace(-2,2,1024)
-  avar(m)=2*sum(Sy(1:end-1).*(fFint(2:end)-fFint(1:end-1)).*sin(4*pi*tau*fFint(1:end-1)).^4./(4*pi*tau*fFint(1:end-1)).^2);
-  m=m+1;
-end
-
-% plot resulting adev=sqrt(avar)
-adev=sqrt(avar);
-loglog(logspace(-2,2,1024),adev/fc)
-xlabel('tau (s)');ylabel('ADEV (no unit)')
-grid on
+legend('Class I','GM class II','BC class II')
